14a081e0d0f2720ef88b7b304f303e0e37b0b0a9
Valentina Prado <valenpschool@gmail.com>
2024-09-20 15:38:42 -0300

    feat: dia 7

--- src/app/components/pages/ships/ship.component.ts
+++ src/app/components/pages/ships/ship.component.ts
@@ -1,11 +1,14 @@
 import { CommonModule } from '@angular/common';
+import {trigger, state, style, transition, animate} from '@angular/animations'
 import { Component, inject } from '@angular/core';
-import { MatTableModule } from '@angular/material/table';
+import { MatTableModule, MatCell } from '@angular/material/table';
 import { FormsModule } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInput } from '@angular/material/input';
-import { MatPaginatorModule, PageEvent, MatPaginatorIntl } from '@angular/material/paginator';
+import { MatIcon } from '@angular/material/icon';
+import { MatPaginatorModule, PageEvent } from '@angular/material/paginator'
+import { MatDialogModule } from '@angular/material/dialog';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { SwapiService } from '../../../services/swapi.service';
 import { Ship } from '../../../types/ship';
@@ -16,11 +19,21 @@ import { Ship } from '../../../types/ship';
     CommonModule,
     FormsModule,
     MatButtonModule,
+    MatCell,
+    MatDialogModule,
     MatFormFieldModule,
     MatInput,
     MatTableModule,
     MatPaginatorModule,
+    MatIcon,
     MatProgressSpinnerModule,
+  ],
+  animations: [
+    trigger('detailExpand', [
+      state('collapsed,void', style({height: '0px', minHeight: '0'})),
+      state('expanded', style({height: '*'})),
+      transition('expanded <=> collapsed', animate('225ms cubic-bezier(0.4, 0.0, 0.2, 1)')),
+    ]),
   ],
   templateUrl: './ships.component.html',
   styleUrl: './ships.component.css',
@@ -31,12 +44,15 @@ export class ShipsComponent {
   isLoading: boolean = false;
   currentPage: number = 0
   searchInput: string = '';
+  showDetails: boolean = false
+  expandedElement: Ship | null = null
   displayedColumns: string[] = [
     'name',
     'model',
     'manufacturer',
     'cargo_capacity',
   ];
+  columnsToDisplayWithExpand = [...this.displayedColumns, 'expand'];
 
   length = 0;
   pageSize = 10;
@@ -45,12 +61,12 @@ export class ShipsComponent {
 
 
   get(page = 1) {
-    console.log(MatPaginatorIntl.prototype)
     this.isLoading = true
     this.shipService.getAllShips(page).subscribe((res) => {
       this.dataSource = res.results;
       this.length = res.count
       this.isLoading = false
+
     });
   }
   ngOnInit() {
@@ -58,7 +74,6 @@ export class ShipsComponent {
   }
 
   searchShip() {
-    console.log(this.searchInput);
     this.isLoading = true;
     this.shipService.searchShips(this.searchInput).subscribe((res) => {
       this.dataSource = res.results;
@@ -73,6 +88,7 @@ export class ShipsComponent {
     this.currentPage = this.pageIndex + 1
     this.get(this.currentPage)
   }
+
 
 
 }
